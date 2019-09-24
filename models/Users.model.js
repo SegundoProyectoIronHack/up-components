9c4362f8941e6f4f92e4e90a5535f22dde1b61aa
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-09-24 23:28:03 +0200

    Fix the Users model problem with email

--- models/Users.model.js
+++ models/Users.model.js
@@ -7,10 +7,6 @@ const userSchema = new Schema({
       type: String,
       unique: true
     },
-    email: {
-      type: String,
-      unique: true
-    },
     password: String,
     name: String,
     surname: String,
