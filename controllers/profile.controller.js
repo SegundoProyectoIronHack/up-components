480ceadd4c3bce02d68dbfaec940ef9c2f2c7b4a
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-09-25 16:24:22 +0200

    Update profile done

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,18 +1,70 @@
-const controller = {}
-const Users = require("./../models/Users.model")
-const appCodes = require("./../app-codes/app-codes")
+const controller = {};
+const Users = require("./../models/Users.model");
+const appCodes = require("./../app-codes/app-codes");
 
 controller.getProfile = (req, res, next) => {
-  Users.findById(req.params.userId)
-  .then(userFound => {
+  Users.findById(req.params.userId).then(userFound => {
     if (userFound) {
-      res.render("auth/profile/index", {message: appCodes[`${req.query.message}`]})
+      res.render("auth/profile/index", {
+        message: appCodes[`${req.query.message}`]
+      });
     }
-  })
-}
+  });
+};
 
 controller.updateProfile = (req, res, next) => {
-  res.redirect(`/profile/${req.params.userId}?message=7`)
-}
+  let {
+    name,
+    surname,
+    email,
+    phone,
+    address,
+    buildingNumber,
+    zipCode,
+    suiteNumber,
+    country,
+    stateProvince,
+    city
+  } = req.body;
 
-module.exports = controller
+  console.log(
+    name,
+    surname,
+    email,
+    phone,
+    address,
+    buildingNumber,
+    zipCode,
+    suiteNumber,
+    country,
+    stateProvince,
+    city
+  );
+
+  Users.findByIdAndUpdate(
+    req.params.userId,
+    {
+      "profile.name": name,
+      "profile.surname": surname,
+
+      "contact.email": email,
+      "contact.phone": phone,
+      
+      "address.address": address,
+      "address.buildingNumber": buildingNumber,
+      "address.zipCode": zipCode,
+      "address.suiteNumber": suiteNumber,
+      "address.country": country,
+      "address.stateProvince": stateProvince,
+      "address.city": city
+    },
+    {
+      new: true
+    }
+  ).then(userUpdated => {
+    console.log(userUpdated);
+    res.redirect(`/profile/${req.params.userId}?message=7`);
+  });
+};
+
+module.exports = controller;
