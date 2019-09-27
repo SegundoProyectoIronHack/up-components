4e516cd3b662d38546efa550d3558f46d7cd7d7c
José Luis Valderrama Regueira <pepe.valderrama.regueira@gmail.com>
2019-09-27 13:56:50 +0200

    Keep styling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
+let loginLink = document.getElementById("login-link") || undefined
+let loginLightbox = document.getElementById("login-lightbox") || undefined
+let signupLink = document.getElementById("signup-link") || undefined
+let signupLightbox = document.getElementById("signup-lightbox") || undefined
 
-  console.log('IronGenerator JS imported successfully!');
+if (loginLink) {
+  loginLink.addEventListener('click', e => {
+    loginLightbox.classList.add("lightbox-visible")
+  })
+}
 
-}, false);
+if (loginLightbox) {
+  loginLightbox.addEventListener('click', e => {
+    if (e.target.classList.contains('lightbox')) {
+      loginLightbox.classList.remove("lightbox-visible")
+    }
+  })
+}
+
+if (signupLink) {
+  signupLink.addEventListener('click', e => {
+    signupLightbox.classList.add("lightbox-visible")
+  })
+}
+
+if (signupLightbox) {
+  signupLightbox.addEventListener('click', e => {
+    if (e.target.classList.contains('lightbox')) {
+      signupLightbox.classList.remove("lightbox-visible")
+    }
+  })
+}
